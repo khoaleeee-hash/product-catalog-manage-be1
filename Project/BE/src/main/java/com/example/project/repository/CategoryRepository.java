4e93f1e2edf299c607838471360ae6f6aa9b2c72
Điền Trần <dientran0410@gmail.com>
2026-01-24 10:06:15 +0700

    fix CategoryRepository class

--- Project/BE/src/main/java/com/example/project/repository/CategoryRepository.java
+++ Project/BE/src/main/java/com/example/project/repository/CategoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository {
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
